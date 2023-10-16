6f46f25fc26feaf4a51683bbfad1dc7bea32ff5d
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-16 09:28:53 -0300

    add Header component

--- App.tsx
+++ App.tsx
@@ -6,12 +6,18 @@ import { Loading } from '@components/Loading';
 import theme from './src/theme';
 
 import { ThemeProvider } from 'styled-components';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}> 
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
