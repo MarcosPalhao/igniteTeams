8c69582769e8aaa32c64e3b01a80d312bef51756
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-17 14:02:25 -0300

    add ButtonIcon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
@@ -12,7 +13,7 @@ export function Players() {
                 subtitle="adicione a galera e separe os times"
             />
 
-            
+            <ButtonIcon />
         </Container>
     )
 }
