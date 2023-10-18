1b03381841633ca14f2352e684a4f5f105358f78
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-18 10:55:44 -0300

    get Params

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,18 +8,27 @@ import { ListEmpty } from "@components/ListEmpty";
 import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { useRoute } from '@react-navigation/native';
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
+type RouteParams = {
+    group: string;
+}
 
 export function Players() {
     const [team, setTeam] = useState('Time A');
     const [players, setPlayers] = useState([]);
+
+    const route = useRoute();
+
+    const { group } = route.params as RouteParams;
 
     return (
         <Container>
             <Header showBackButton />
 
             <Highlight 
-                title="Nome da turma"
+                title={group}
                 subtitle="adicione a galera e separe os times"
             />
 
